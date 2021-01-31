c060c1dbc9ee5a6301622335e3c4191a09160fd3
Hari K Kapparapu <hari.kapparapu@alcon.com>
2021-01-30 23:04:57 -0800

    Rename application

--- application/windows/scanner_app/src/main.cpp
+++ application/windows/scanner_app/src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-#include "barcode_scanner/barcode_scanner_engine.h"
-#include "opencv_windows/barcode_scanner.h"
-#include "opencv_windows/video_camera.h"
+#include "barcode_scanner_engine.h"
 #include "tchain/types.h"
 
 /* ------------------------------------------------------------------------ */
@@ -10,12 +8,15 @@
 int main() {
   printf("~~~~ App Start ...\n");
 
-  ::hal::WindowsVideoCamera video_cam = {
-      ::hal::WindowsVideoCamera::Port::PORT_1};
-  ::hal::WindowsBarcodeScanner barcode_scanner = {};
-  ::engine::BarcodeScannerEngine scanner_engine = {video_cam, barcode_scanner};
+  BarcodeScannerEngine barcode_scanner_engine = {};
 
-  scanner_engine.ScanStart();
+  // ::hal::WindowsVideoCamera video_cam = {
+  //     ::hal::WindowsVideoCamera::Port::PORT_1};
+  // ::hal::WindowsBarcodeScanner barcode_scanner = {};
+  // ::engine::BarcodeScannerEngine scanner_engine = {video_cam,
+  // barcode_scanner};
+
+  // scanner_engine.ScanStart();
 
   printf("~~~~ Closed ...\n");
 }
